18c688818e34756ca2fb4f6b20ca619d36c77e95
jragon <rory@jragon.co.uk>
2013-11-24 14:32:41 +0000

    Tests now pass

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,5 +1,7 @@
 class Fruit < ActiveRecord::Base
   belongs_to :conversation
+  has_many :threats
+  has_many :contributors
 
   validates :conversation_id, :name, :rank, :ten_seed, presence: true
 end
--- app/models/threat.rb
+++ app/models/threat.rb
@@ -1,2 +1,5 @@
 class Threat < ActiveRecord::Base
+  belongs_to :fruit
+
+  validates :fruit_id, :name, :rank, :ten_seed, presence: true
 end
