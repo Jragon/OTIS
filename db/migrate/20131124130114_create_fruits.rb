2e5d8aaeabd68e5ea1dee874c76994e3f4e40951
jragon <rory@jragon.co.uk>
2013-11-24 13:03:28 +0000

    spelled 'conversation' wrong, oops

--- db/migrate/20131124130114_create_fruits.rb
+++ db/migrate/20131124130114_create_fruits.rb
@@ -1,7 +1,7 @@
 class CreateFruits < ActiveRecord::Migration
   def change
     create_table :fruits do |t|
-      t.integer :convosation_id
+      t.integer :conversation_id
       t.integer :rank
       t.integer :ten_seed
       t.text :observations
