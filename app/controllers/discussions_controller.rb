59b5df41ac56e2875e91c1d7cec6c66af3cc5f98
jragon <rory@jragon.co.uk>
2013-12-15 20:35:36 +0000

    Fixed, hopefully, destroy

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -8,6 +8,17 @@ class DiscussionsController < InheritedResources::Base
     end
   end
 
+  def update
+    @discussion = Discussion.find(params[:id])
+    @discussion.update_attributes(discussion_params)
+    
+    respond_with_bip(@discussion)
+  end
+
+  def destroy
+    destroy! { [@village.programme, @village] }
+  end
+
   protected
     def collection
       if @village
@@ -26,10 +37,16 @@ class DiscussionsController < InheritedResources::Base
     def set_village
       if params[:village_id]
         @village = Village.find(params[:village_id])
+      elsif params[:id]
+        @village = Discussion.find(params[:id]).village
       end
     end
 
     def permitted_params
-      params.permit(discussion: [:lead_facilitator, :participants, :duration, :date_held, :group_name, :village_id])
+      params.permit(discussion: [:lead_facilitator, :participants, :duration, :date_held, :group_name, :group_id, :village_id])
+    end
+
+    def discussion_params
+      params.require(:discussion).permit(:lead_facilitator, :participants, :duration, :date_held, :group_name, :group_id, :village_id)
     end
 end
